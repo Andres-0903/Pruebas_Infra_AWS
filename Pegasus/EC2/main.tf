e00496242aea6e9d1f708d416b338e9c25c7fc0b
Andres <arenasandres7@gmail.com>
2025-12-18 13:29:46 -0500

    update main.tf

--- Pegasus/EC2/main.tf
+++ Pegasus/EC2/main.tf
@@ -28,3 +28,49 @@ resource "aws_subnet" "public_subnet" {
   depends_on              = [aws_vpc.vpc_virginia]
   map_public_ip_on_launch = true
 }
+
+###
+#Datos Internet Gateway
+##
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.vpc_virginia.id
+  tags = {
+    "Name" = "Internet_gateway"
+  }
+}
+
+###
+#Datos Route Table
+##
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.vpc_virginia.id
+  tags = {
+    "Name" = "Route_table"
+  }
+}
+
+resource "aws_route" "route" {
+  route_table_id         = aws_route_table.route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.internet_gateway.id
+}
+
+###
+#Datos Route Table Association
+##
+resource "aws_route_table_association" "public_route_table_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+###
+#Datos instancia Ec2
+###
+resource "aws_instance" "public_instance" {
+  ami           = "ami-0a887e37c04b31207"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnet.id
+  tags = {
+    "Name" = "public_instance"
+  }
+}
