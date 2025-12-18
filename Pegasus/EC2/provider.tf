b4e759e60b3458aa164c4e1d7d816466a59c30c6
Andres <arenasandres7@gmail.com>
2025-12-18 09:35:55 -0500

    add pegasus

---
+++ Pegasus/EC2/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.100.0"
+    }
+  }
+  required_version = ">= 1.13.4"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
