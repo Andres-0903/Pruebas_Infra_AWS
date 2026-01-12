afe099ee56ee21f2e70543a6b982f3ab4105af02
Andres <arenasandres7@gmail.com>
2026-01-12 16:43:07 -0500

    update module3

--- Pegasus/EC2/monitoreo.tf
+++ Pegasus/EC2/monitoreo.tf
@@ -4,7 +4,7 @@
 # }
 
 module "ec2_monitoring" {
-  source = "git::https://github.com/Andres-0903/Infra_AWS_Module_Monitoring_EC2.git?ref=1.0.1"
+  source = "git::https://github.com/Andres-0903/Infra_AWS_Module_Monitoring_EC2.git?ref=1.0.2"
 
   ec2_instances = ["i-09e760154b7e7c682", "i-09e760154b7e7c682", "i-089305ba0767eb28d"]
   project       = "monitoreo"
