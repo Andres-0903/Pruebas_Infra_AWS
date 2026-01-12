1a92ad080fed951caf08978525d6e0f875275b3c
Andres <arenasandres7@gmail.com>
2026-01-12 15:45:11 -0500

    update module

---
+++ Pegasus/EC2/monitoreo.tf
@@ -0,0 +1,8 @@
+# module "ec2_instances" {
+#     source = "https://github.com/Andres-0903/Infra_AWS_Module_Monitoring_EC2/tree/1.0.1"  
+
+# }
+
+module "ec2_monitoring" {
+  source = "git::https://github.com/Andres-0903/Infra_AWS_Module_Monitoring_EC2.git?ref=1.0.1"
+}
