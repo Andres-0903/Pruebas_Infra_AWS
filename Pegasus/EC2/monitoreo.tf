0bc5de4be724708efa40ecd5908f1435649ef2b2
Andres <arenasandres7@gmail.com>
2026-01-13 17:52:34 -0500

    update module18

--- Pegasus/EC2/monitoreo.tf
+++ Pegasus/EC2/monitoreo.tf
@@ -1,8 +1,8 @@
-# module "ec2_monitoring" {
-#   source = "git::https://github.com/Andres-0903/Infra_AWS_Module_Monitoring_EC2.git?ref=1.0.3"
+module "ec2_monitoring" {
+  source = "git::https://github.com/Andres-0903/Infra_AWS_Module_Monitoring_EC2.git?ref=1.0.3"
 
-#   ec2_instances = local.ec2_instances
-#   project       = "monitoreo"
-#   environment   = "dev"
-#   sns_topic_arn = var.sns_topic_arn
-# }
+  ec2_instances = local.ec2_instances
+  project       = "monitoreo"
+  environment   = "dev"
+  sns_topic_arn = var.sns_topic_arn
+}
