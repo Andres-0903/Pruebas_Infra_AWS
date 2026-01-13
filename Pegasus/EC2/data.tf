69f6522c627695efe6a7846b56896025d2494892
Andres <arenasandres7@gmail.com>
2026-01-13 16:06:15 -0500

    update module13

--- Pegasus/EC2/data.tf
+++ Pegasus/EC2/data.tf
@@ -2,7 +2,7 @@ data "aws_key_pair" "my_key" {
   key_name = "my_key"
 }
 
-data "aws_instance" "monitoreo" {
+data "aws_instance" "public_instance" {
   filter {
     name   = "tag:Name"
     values = ["monitoreo1", "monitoreo2", "monitoreo3"]
