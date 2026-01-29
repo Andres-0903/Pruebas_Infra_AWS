9018933d108eec3b9d6b2a06cb361596eabfdf49
Andres <arenasandres7@gmail.com>
2026-01-29 14:56:24 -0500

    actualizar5

--- Pegasus/EC2/cloudwatch_alarms.tf
+++ Pegasus/EC2/cloudwatch_alarms.tf
@@ -8,6 +8,6 @@ module "cloudwatch_alarms_silence" {
     "monitoreo-EC2-CPUUtilization-MongoDB-dev"
   ]
 
-  mute_cron   = "cron(00 20 * * ? *)" # 03:00 PM
-  unmute_cron = "cron(10 20 * * ? *)" # 03:10 PM
+  mute_cron   = "cron(15 20 * * ? *)" # 03:15 PM
+  unmute_cron = "cron(30 20 * * ? *)" # 03:30 PM
 }
